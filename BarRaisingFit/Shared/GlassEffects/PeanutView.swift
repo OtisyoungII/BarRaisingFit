8c647e53937d8502afa65428c1b760a993ae9eaf
Otis YII <oyoung21@gmail.com>
2025-07-19 23:11:42 -0400

    Peanut CHeck

---
+++ BarRaisingFit/Shared/GlassEffects/PeanutView.swift
@@ -0,0 +1,18 @@
+//
+//  PeanutView.swift
+//  BarRaisingFit
+//
+//  Created by Otis Young on 7/19/25.
+//
+
+import SwiftUI
+
+struct PeanutView: View {
+    var body: some View {
+        Text("Do you have my Peanuts")
+    }
+}
+
+#Preview {
+    PeanutView()
+}
